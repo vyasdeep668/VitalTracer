388ba49b717dc60f601d5f2f2ed022e9dfbcf8e0
Deep Vyas <vyasdeep668@gmail.com>
2023-05-11 02:41:43 -0600

    Minor Changes in Algorithm

--- 3_HR_Detection_Algorithm/HeartRateDetectionScript.py
+++ 3_HR_Detection_Algorithm/HeartRateDetectionScript.py
@@ -13,13 +13,13 @@ def measureHeartRate(ppg_data):
     """
     measureHeartRate: This Function will measure HeartRate from PPG Raw Data
     :param in -  ppg_data: List/Dataframe Column of Raw PPG Values
-    :param out - heartRate: Heart Rate in bpm
+    :param out - heartRate: Heart Rate in bpm or 0(Error)
     """ 
 
     # Step1: Filter the PPG signal with a bandpass filter
     # Low-pass filter the raw signal to remove noise
-    fs = 100  # Sampling frequency
-    fc =  1.5 # Cutoff frequency
+    fs = 500  # Sampling frequency
+    fc =  5 # Cutoff frequency
     b, a = butter(2, 2 * fc / fs, 'low')
 
     ppg_data_raw = ppg_data
@@ -51,19 +51,34 @@ def measureHeartRate(ppg_data):
 
     # Step5:  Integrate the squared signal with a sliding window.
     # Apply a moving average integration to smooth the signal
-    window_size = int(0.5 * fs)  # 50 window size
+    window_size = int(0.1 * fs)  # 50 Samples window size
     window = np.ones(window_size) / float(window_size)
     ppg_data = np.convolve(ppg_data, window, "same")
 
     # Step6: Find the R-peaks in the integrated signal.
     # Find peaks in the integrated signal
     ppg_peaks, _ = find_peaks(ppg_data, distance=0.2*fs)
+    # Get a list of Amplitude of peaks
+    ppg_peaks_amplitudes = [ppg_data[i] for i in ppg_peaks]
+    max_peak_amplitude = max(ppg_peaks_amplitudes)
+    PEAK_AMPLITUDE_THRESHOLD = 0.20*max_peak_amplitude
+    ppg_peaks_refined = []
+    for ppg_peak in ppg_peaks:
+        if(ppg_data[ppg_peak] > PEAK_AMPLITUDE_THRESHOLD):
+            # Amplitude is big enough to be considered as a peak
+            ppg_peaks_refined.append(ppg_peak)
 
     # Step7: Calculate the heart rate using the time difference between R-peaks.
     # Compute the inter-beat interval (IBI) and heart rate (HR) from the peak locations
-    ibi = np.diff(ppg_peaks) / fs  # IBI in seconds
+    ibi = np.diff(ppg_peaks_refined) / fs  # IBI in seconds
     hr = 60 / ibi  # HR in bpm
-    return round(np.mean(hr), 2)
+    mean_hr = np.mean(hr)
+    HEARTRATE_LOWER_THRESHOLD = 25
+    HEARTRATE_HIGHER_THRESHOLD = 300
+    if (mean_hr < HEARTRATE_LOWER_THRESHOLD) or (mean_hr > HEARTRATE_HIGHER_THRESHOLD):
+        return 0 # Error 
+    else:        
+        return round(mean_hr, 2)
 
 
 
